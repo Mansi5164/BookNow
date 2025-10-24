b409f7e3ea48f7ba7f12c5be423c38826b7f1320
Ludoviko451 <luisangelquirosflorez@gmail.com>
2025-10-24 11:11:53 -0500

    chore: remove unused import statement (#19)

--- src/main/java/com/cabbooking/controller/LocationController.java
+++ src/main/java/com/cabbooking/controller/LocationController.java
@@ -4,7 +4,6 @@ import com.cabbooking.entity.LocationLog;
 import com.cabbooking.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
